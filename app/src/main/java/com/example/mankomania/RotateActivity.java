e24fd02823e75ce78e2fe94da242cf7bded2d038
Nicole <nicolebu@edu.aau.at>
2019-04-18 14:19:24 +0200

    implemented gesture

--- app/src/main/java/com/example/mankomania/RotateActivity.java
+++ app/src/main/java/com/example/mankomania/RotateActivity.java
@@ -1,7 +1,11 @@
 package com.example.mankomania;
 
+import android.annotation.SuppressLint;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
@@ -18,18 +22,31 @@ public class RotateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rotate);
 
         imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int touch = MotionEventCompat.getActionMasked(event);
 
-        rotateAnimation();
+                switch (touch){
+                    case (MotionEvent.ACTION_MOVE):
+                        rotateAnimation();
+                        return true;
+                    }
+                    return true;
+                }
+        });
     }
 
     private void rotateAnimation() {
         rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
         imageView.startAnimation(rotateAnimation);
         rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
 
-            }
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
 
             @Override
             public void onAnimationEnd(Animation animation) {
@@ -50,13 +67,7 @@ public class RotateActivity extends AppCompatActivity {
                     }
                 },2000);
             }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
         });
-
         }
 
     public void openPopUp() {
