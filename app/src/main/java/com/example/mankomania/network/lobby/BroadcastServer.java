2cf73884b6d2b4db6f861506cea52fa89b9b6898
Timon Wösch <woesch09@gmail.com>
2019-05-17 10:20:05 +0200

    Lobby Server returns now 4x an answer

--- app/src/main/java/com/example/mankomania/network/lobby/BroadcastServer.java
+++ app/src/main/java/com/example/mankomania/network/lobby/BroadcastServer.java
@@ -12,15 +12,19 @@ public class BroadcastServer extends Thread {
     @Override
     public void run() {
         try (DatagramSocket socket = new DatagramSocket(4445)){
+            int i = 0;
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
+            while(i<4) {
+                socket.receive(packet);
 
-            InetAddress address = packet.getAddress();
-            packet = new DatagramPacket(buf, buf.length, address, 4446);
+                InetAddress address = packet.getAddress();
+                packet = new DatagramPacket(buf, buf.length, address, 4446);
 
-            Thread.sleep(200);
+                Thread.sleep(200);
 
-            socket.send(packet);
+                socket.send(packet);
+                i++;
+            }
         }catch (Exception e){
             Log.e("BroadcastServer",""+e);
         }
