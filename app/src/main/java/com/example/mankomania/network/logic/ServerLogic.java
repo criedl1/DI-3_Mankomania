c2bf411d2ff6eba8ccc0839593f4a074c6140358
Timon Wösch <woesch09@gmail.com>
2019-05-20 16:03:11 +0200

    Constructed Server Logic

---
+++ app/src/main/java/com/example/mankomania/network/logic/ServerLogic.java
@@ -0,0 +1,55 @@
+package com.example.mankomania.network.logic;
+
+public class ServerLogic {
+    private ServerLogicStates sls;
+    private int player;
+
+    public ServerLogic() {
+        this.sls = ServerLogicStates.END;
+    }
+
+    public boolean startTurn(int player) {
+        if (sls == ServerLogicStates.END) {
+            sls = ServerLogicStates.BEGIN;
+            this.player = player;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean throwDice(int player){
+        if(sls == ServerLogicStates.BEGIN && this.player == player){
+            sls = ServerLogicStates.ACTION;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean doAction(int player){
+        if(sls == ServerLogicStates.ACTION && this.player == player){
+            sls = ServerLogicStates.CHANGES;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean changeValue(int player){
+        if(sls == ServerLogicStates.CHANGES && this.player == player){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean endTurn(int player){
+        if(sls == ServerLogicStates.CHANGES && this.player == player){
+            sls = ServerLogicStates.END;
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mankomania/network/logic/ServerLogicStates.java
@@ -0,0 +1,8 @@
+package com.example.mankomania.network.logic;
+
+public enum ServerLogicStates {
+    END,
+    BEGIN,
+    ACTION,
+    CHANGES
+}
