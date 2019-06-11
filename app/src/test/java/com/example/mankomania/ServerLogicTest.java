f920382050d3074f3efb791c083dec079aa4ed2b
Timon Wösch <woesch09@gmail.com>
2019-06-11 14:14:15 +0200

    Wrong Player Tests

--- app/src/test/java/com/example/mankomania/ServerLogicTest.java
+++ app/src/test/java/com/example/mankomania/ServerLogicTest.java
@@ -195,4 +195,164 @@ public class ServerLogicTest {
         serverLogic.startTurn(0);
         Assert.assertFalse(serverLogic.endTurn(0));
     }
+
+
+
+    //Wrong player Tests ##############################################
+    //Base Tests
+    @Test
+    public void throwDiceWrongPlayer() {
+        serverLogic.startTurn(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void doActionWrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        Assert.assertFalse(serverLogic.doAction(1));
+    }
+    @Test
+    public void changeValueWrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        Assert.assertFalse(serverLogic.changeValue(1));
+    }
+    @Test
+    public void endTurnWrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
+    @Test
+    public void standardTest1WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.changeValue(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
+    @Test
+    public void standardTest2WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.doAction(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
+    @Test
+    public void standardTest3WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.changeValue(0);
+        serverLogic.doAction(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
+    @Test
+    public void standardTest4WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.doAction(0);
+        serverLogic.changeValue(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
+
+    //Double Call Fail Tests
+    @Test
+    public void startTurnDoubleWrongPlayer() {
+        serverLogic.startTurn(0);
+        Assert.assertFalse(serverLogic.startTurn(1));
+    }
+    @Test
+    public void throwDiceDoubleWrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void endTurnDoubleWrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.endTurn(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
+
+    //Fail Throw Tests
+    @Test
+    public void failThrowDice1WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void failThrowDice2WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.doAction(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void failThrowDice3WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.changeValue(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void failThrowDice4WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.changeValue(0);
+        serverLogic.doAction(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void failThrowDice5WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.doAction(0);
+        serverLogic.changeValue(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+    @Test
+    public void failThrowDice6WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.doAction(0);
+        serverLogic.changeValue(0);
+        serverLogic.endTurn(0);
+        Assert.assertFalse(serverLogic.throwDice(1));
+    }
+
+    //Fail DoAction Tests
+    @Test
+    public void failDoAction1WrongPlayer() {
+        serverLogic.startTurn(0);
+        Assert.assertFalse(serverLogic.doAction(1));
+    }
+    @Test
+    public void failDoAction2WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.endTurn(0);
+        Assert.assertFalse(serverLogic.doAction(1));
+    }
+
+    //Fail DoAction Tests
+    @Test
+    public void failChangeValue1WrongPlayer() {
+        serverLogic.startTurn(0);
+        Assert.assertFalse(serverLogic.changeValue(1));
+    }
+    @Test
+    public void failChangeValue2WrongPlayer() {
+        serverLogic.startTurn(0);
+        serverLogic.throwDice(0);
+        serverLogic.endTurn(0);
+        Assert.assertFalse(serverLogic.changeValue(1));
+    }
+
+    //Fail EndTurn Tests
+    @Test
+    public void failEndTurn1WrongPlayer() {
+        serverLogic.startTurn(0);
+        Assert.assertFalse(serverLogic.endTurn(1));
+    }
 }
