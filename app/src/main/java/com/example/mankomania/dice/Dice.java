da88ebab5ea9488c7ca04d48526c4828ffa86b26
Joniras <jonas.schaltegger@gmail.com>
2019-06-18 13:54:37 +0200

    Dice fix

--- app/src/main/java/com/example/mankomania/dice/Dice.java
+++ app/src/main/java/com/example/mankomania/dice/Dice.java
@@ -59,7 +59,7 @@ public class Dice extends Fragment implements SensorEventListener {
         // sum of sensors
         float acceleration = (x + y + z);
         // if sum > 70 --> roll the Dice
-        if (acceleration > 10 && !diceRolled) {
+        if (acceleration > 15 && !diceRolled) {
             diceRolled = true;
             ((MapView) getActivity()).sendRollDice();
         }
